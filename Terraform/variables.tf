09a6b1ce3f03c1899da89fa803090efc58390f91
jagadish valluri <jagadishvalluri64@gmail.com>
2026-03-10 12:55:58 +0530

    changed default key file path suiting spacelift context

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "key_file" {
     description = "This is the public key for ansible automation with spacelift"
     type = string
-    default = "C:/Users/91949/Desktop/keys/spacetf.key.pub"
+    default = "/mnt/workspace/spacetf.key.pub"
 
 }
